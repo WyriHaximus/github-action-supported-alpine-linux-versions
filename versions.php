c47067c447aa748905b8f8702ec0052e5ab457a6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-10-02 21:31:55 +0200

    Add App

---
+++ versions.php
@@ -0,0 +1,31 @@
+<?php
+
+$versions = [];
+
+$d = new DOMDocument();
+@$d->loadHTML(file_get_contents('https://wiki.alpinelinux.org/wiki/Alpine_Linux:Releases'));
+
+/** @var DOMNode $row */
+foreach ((new DOMXPath($d))->query('//tr') as $row) {
+    if (trim($row->childNodes->item(1)->textContent) === 'Branch') {
+        continue;
+    }
+
+    if (trim($row->childNodes->item(1)->textContent) === 'edge') {
+        continue;
+    }
+
+    $version = substr(trim($row->childNodes->item(1)->textContent), 1);
+    if (trim($row->childNodes->item(11)->textContent) === 'on request only') {
+        continue;
+    }
+
+    $versions[] = $version;
+
+    if (count($versions) >= 3) {
+        break;
+    }
+}
+
+echo 'Found the following versions: ', implode(', ', $versions), PHP_EOL;
+echo '::set-output name=versions::', json_encode($versions), PHP_EOL;
